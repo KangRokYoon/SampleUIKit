94ee431c5490d04cb0e9ffa6bfe68af1b35f02e8
KangRok Yoon <ykr0919@naver.com>
2023-09-16 22:55:33 +0900

    Initial Commit

---
+++ SampleUIKit/MySampleLabel.swift
@@ -0,0 +1,20 @@
+//
+//  MySampleLabel.swift
+//  SampleUIKit
+//
+//  Created by 윤강록 on 2023/09/16.
+//
+
+import UIKit
+
+class MySampleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ SampleUIKit/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SampleUIKit
+//
+//  Created by 윤강록 on 2023/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+}
+
+extension ViewController: UITabBarDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+extension ViewController: UITableViewDataSource {
+        
+    
+}
